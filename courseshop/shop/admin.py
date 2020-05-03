058c9e3cca79d9ab3985b851ad78ab90db5b0a27
bossniger <egorbelyakovich2@gmail.com>
2020-05-04 00:46:03 +0300

    ЧВ

---
+++ courseshop/shop/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin,)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available'] # редактируем на странице отображения)
+    prepopulated_fields = {'slug': ('name',)} #значение автоматом с использованием других полей
+
+
+admin.site.register(Product, ProductAdmin)
+# Register your models here.
